99bf2ffaa97835941707b241196d50fd71f3dc7a
vertibtw <btwverti@gmail.com>
2026-06-22 15:22:48 +0200

    misunderstood a guy on the nixos discord

--- home/verti/packages/wm/wayland/hyprland.nix
+++ home/verti/packages/wm/wayland/hyprland.nix
@@ -1,6 +1,5 @@
 { pkgs, inputs, ... }:
-{
-    # not installing as a hm module cuz it was breaking and I do not have attention span to fix it
+{ 
     home.packages = [
         inputs.hyprland-git.packages.${pkgs.system}.hyprland
     ];
