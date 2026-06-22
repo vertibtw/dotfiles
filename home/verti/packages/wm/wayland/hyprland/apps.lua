60d9c7765a0f7a7df3d7a894727bb5f5869db3d8
vertibtw <btwverti@gmail.com>
2026-06-22 15:15:35 +0200

    this is actually not needed

--- home/verti/packages/wm/wayland/hyprland/apps.lua
+++ home/verti/packages/wm/wayland/hyprland/apps.lua
@@ -7,8 +7,4 @@ M.menu        = "tofi-drun"
 M.menu_args   = "--fuzzy-match true | bash"
 M.screenshot  = 'grim -g "$(slurp)" - | convert - -resize 200% png:- | wl-copy'
 
-function M.init ()
-  -- this is needed because of the automated file inclusion
-end
-
 return M
