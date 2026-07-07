15349a43ac96cf689f8e4165c094efcdfe60362b
vertibtw <btwverti@gmail.com>
2026-07-07 20:13:57 +0200

    pixelated fonts waow

---
+++ modules/nixos/fonts/default.nix
@@ -0,0 +1,3 @@
+{
+    imports = [ ./fontconfig.nix ./fontconfig.nix ];
+}
---
+++ modules/nixos/fonts/fonts.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+    fonts.packages = with pkgs; [
+        scientifica
+        nerd-fonts.jetbrains-mono
+        inter
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-emoji
+    ];
+}
