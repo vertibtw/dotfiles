1e50cd9fd5779b275e3bdaaf1ba94ebed8fe5a87
vertibtw <btwverti@gmail.com>
2026-07-11 13:33:15 +0200

    dunst work now

--- home/verti/packages/gui/dunst.nix
+++ home/verti/packages/gui/dunst.nix
@@ -4,12 +4,34 @@
         enable = true;
         settings = {
             global = {
-                origin = "center";
-                width = "(300,200)";
-                height = "(0,150)";
-                transparency = 10;
+                origin = "top-center";
+                offset = "(200,0)";
+                width = "(200,200)";
+                height = "(100,150)";
+                frame_width = 1;
                 frame_color = theme.colors.text1;
             };
+
+            urgency_normal = {
+                background = theme.colors.background;
+                foreground = theme.colors.text1;
+                timeout = 10;
+            };
+
+            urgency_critical = {
+                background = theme.colors.background;
+                foreground = theme.colors.text1;
+                timeout = 10;
+                frame_color = theme.colors.red;
+            };
+
+            urgency_low = {
+                background = theme.colors.background;
+                foreground = theme.colors.text2;
+                timeout = 5;
+                frame_color = theme.colors.background;
+            };
+
         };
     };
 }
--- modules/nixos/services/open-webui.nix
+++ modules/nixos/services/open-webui.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
     services.open-webui = {
-      enable = true;
+      enable = false;
       port = 6767;
       environment = {
         WEBUI_AUTH = "False";
