615a05baa7b1f375375adc258a72e728440f7c3f
vertibtw <btwverti@gmail.com>
2026-07-17 16:48:01 +0200

    starship stuff

--- home/verti/packages/shell/starship.nix
+++ home/verti/packages/shell/starship.nix
@@ -4,11 +4,21 @@
     enable = true;
     settings = {
       format = ''
-        ''${custom}$directory$git_branch$git_status$character
+        ''${custom}$hostname$directory$git_branch$git_status$character
       '';
       directory = {
         truncation_length = 0;
         fish_style_pwd_dir_length = 1;
+      };
+      custom."user-first-letter" = {
+        command = "whoami | cut -c1";
+        when = true;
+        format = "[$output](bold blue)";
+      };
+      hostname = {
+        ssh_only = false;
+        format = "@[$hostname](bold yellow) ";
+        trim_at = "";
       };
     };
   };
