cf92aa6922d7cff14a1ef767bd754051fbca3378
vertibtw <btwverti@gmail.com>
2026-08-10 09:45:38 +0200

    xdg desktop portal n stuff

--- hosts/reimu/default.nix
+++ hosts/reimu/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ pkgs, inputs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -8,4 +8,15 @@
 
   networking.hostName = "reimu";
   system.stateVersion = "25.11";
+
+  hardware.graphics.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      inputs.hyprland-git.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    config.common.default = [ "hyprland" "gtk" ];
+  };
 }
