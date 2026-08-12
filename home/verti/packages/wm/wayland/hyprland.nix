fe43c4a6e9622dc05e09632132a817df6d97f6a9
vertibtw <btwverti@gmail.com>
2026-08-12 17:11:54 +0200

    nixified the lua stuff no more .lua files in repo yay!

--- home/verti/packages/wm/wayland/hyprland.nix
+++ home/verti/packages/wm/wayland/hyprland.nix
@@ -12,10 +12,240 @@ in
     inputs.hyprland-git.packages.${pkgs.stdenv.hostPlatform.system}.hyprland
   ];
 
-  xdg.configFile."hypr" = {
-    source = ./hyprland;
-    recursive = true;
-  };
+  xdg.configFile."hypr/hyprland.lua".text = ''
+    -- APPS
+
+    local apps = {
+        terminal = "kitty",
+        filemgr     = "dolphin",
+        menu        = "fuzzel",
+        clipboardmgr= "cliphist list | fuzzel --dmenu | cliphist decode | wl-copy",
+        screenshot  = 'grim -g "$(slurp)" - | magick convert - -resize 200% png:- | swappy -f -',
+        lockscreen  = 'hyprlock'
+    }
+
+    -- COLORSCHEME
+
+    local colorscheme = {
+      color1 = "rgb(${rgb theme.colors.blue})",
+      color2 = "rgb(${rgb theme.colors.purple})",
+      inactive = "rgb(${rgb theme.colors.background})"
+    }
+
+    -- ENV
+
+    hl.env("XCURSOR_THEME", "xp-cursor")
+    hl.env("XCURSOR_SIZE", "24")
+    hl.env("HYPRCURSOR_SIZE", "24")
+    hl.env("GTK_THEME", "adw-gtk3-dark")
+    hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+    hl.env("QT5_QPA_PLATFORMTHEME", "qt5ct")
+
+    -- AUTOSTART
+    hl.exec_cmd("kitty -e fastfetch")
+    hl.exec_cmd("awww-daemon")
+    hl.exec_cmd("wl-paste --watch cliphist store")
+    hl.exec_cmd("blueman-applet")
+    hl.exec_cmd("nm-applet")
+    hl.exec_cmd("dunst")
+    hl.exec_cmd("vesktop -m --ozone-platform wayland --enable-blink-features=MiddleClickAutoscroll")
+    hl.exec_cmd("vbar")
+
+    for i = 0, 20 do
+      hl.exec_cmd("hyprctl notify 1 2000 0 hello")
+    end
+
+    -- MONITORS
+    -- I tried to sort of turn of eDP-1 if another monitor is active, it sort of works sometimes
+
+    hl.on("monitor.added", function(mon)
+      if mon.name == "eDP-1" then
+        hl.monitor({
+          output   = "eDP-1",
+          mode     = "preferred",
+          position = "0x0",
+          scale    = "1",
+        })
+      else
+        hl.monitor({
+          output   = "eDP-1",
+          mode     = "preferred",
+          position = "0x0",
+          scale    = "1",
+          disabled = true,
+        })
+        hl.monitor({
+          output   = mon.name,
+          mode     = "preferred",
+          position = "0x0",
+          scale    = 1,
+        })
+      end
+    end)
+
+    hl.on("monitor.removed", function(mon)
+      if mon.name ~= "eDP-1" then
+        hl.monitor({
+          output   = "eDP-1",
+          mode     = "preferred",
+          position = "0x0",
+          scale    = "1",
+        })
+      end
+    end)
+
+    -- ANIMATIONS
+
+    hl.curve("in",      { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
+    hl.curve("out",     { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
+    hl.curve("linear",  { type = "bezier", points = { {0, 0},       {1, 1}       } })
+
+    hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 0.5,  bezier= "in",  style = "slide" })
+    hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 0.5, bezier = "out", style = "slide" })
+    hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "in",  style = "fade" })
+    hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1,  bezier = "out", style = "fade" })
+    hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1, bezier = "out", style = "slide" })
+    hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1, bezier = "in", style = "slide" })
+    hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "linear" })
+
+    hl.config({
+      animations = {
+        enabled = true,
+      },
+      decoration = {
+            rounding       = 1,
+            rounding_power = 2,
+            active_opacity   = 1.0,
+            inactive_opacity = 0.9,
+            shadow = {
+                enabled      = false,
+            },
+            blur = {
+                enabled   = true,
+                size = 8,
+                passes = 3,
+                noise = 0.02
+            },
+        },
+    })
+
+    -- INPUT
+
+    hl.device({
+        name        = "tpps/2-elan-trackpoint",
+        sensitivity = 1,
+    })
+
+    hl.device({
+      name          = "synps/2-synaptics-touchpad",
+      sensitivity   = "0.8"
+    })
+
+    hl.config({
+        input = {
+            kb_layout  = "pl",
+            follow_mouse = 1,
+            sensitivity = 0,
+            repeat_delay = 300,
+            touchpad = {
+                natural_scroll = false,
+            },
+        },
+    })
+
+    -- RULES
+
+    hl.layer_rule({
+        match = { namespace = "v.bar" },
+        blur = true,
+        ignore_alpha = 0.4,
+    })
+
+    -- GENERAL
+
+    hl.config({
+        general = {
+            gaps_in  = 5,
+            gaps_out = 10,
+
+            border_size = 1,
+
+            col = {
+              active_border   = { colors = {colorscheme.color1, colorscheme.color2}, angle = 45 },
+              inactive_border = colorscheme.inactive,
+            },
+
+            resize_on_border = false,
+        },
+        misc = {
+            force_default_wallpaper = 1,
+            disable_hyprland_logo   = true,
+        },
+    })
+
+
+    -- BINDS
+
+    local mainMod = "SUPER"
+
+    hl.bind(mainMod .. "+ SHIFT + RETURN", hl.dsp.exec_cmd(apps.terminal))
+    hl.bind(mainMod .. "+ SHIFT + C", hl.dsp.window.close())
+
+    hl.bind(mainMod .. "+ SHIFT + Q", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+    hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(apps.filemgr))
+    hl.bind(mainMod .. "+ SHIFT + S", hl.dsp.exec_cmd(apps.screenshot))
+    hl.bind(mainMod .. "+ ESCAPE", hl.dsp.exec_cmd(apps.lockscreen))
+
+    local shaders = { "", "/home/verti/.config/hypr/shaders/grayscale.frag", "/home/verti/.config/hypr/shaders/blue_light_filter.frag"}
+    local shaderIdx = 1
+
+    hl.bind(mainMod .. "+ F8", function()
+      shaderIdx = (shaderIdx % #shaders) + 1
+      hl.config({ decoration = { screen_shader = shaders[shaderIdx] } })
+    end)
+
+    hl.bind(mainMod .. "+ C", hl.dsp.exec_cmd(apps.clipboardmgr))
+    hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd('if pgrep -x "' .. apps.menu .. '" > /dev/null; then pkill -x ' .. apps.menu .. '; else ' .. apps.menu .. ' ' .. '; fi'))
+    hl.bind(mainMod .. " + F7", hl.dsp.exec_cmd('find ~/Pictures/Wallpapers -type f | fuzzel --dmenu | xargs awww img')) -- temp
+
+    hl.bind(mainMod .. " + H",  hl.dsp.focus({ direction = "left" }))
+    hl.bind(mainMod .. " + L",  hl.dsp.focus({ direction = "right" }))
+    hl.bind(mainMod .. " + K",  hl.dsp.focus({ direction = "up" }))
+    hl.bind(mainMod .. " + J",  hl.dsp.focus({ direction = "down" }))
+    hl.bind(mainMod .. " + SHIFT + H", hl.dsp.window.move({ direction = "left" }))
+    hl.bind(mainMod .. " + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
+    hl.bind(mainMod .. " + SHIFT + K", hl.dsp.window.move({ direction = "up" }))
+    hl.bind(mainMod .. " + SHIFT + J", hl.dsp.window.move({ direction = "down" }))
+
+    hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+    hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({action = "toggle"}))
+
+    for i = 1, 10 do
+        local key = i % 10
+        hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
+        hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+    end
+
+    hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
+    hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
+
+    hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+    hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
+
+    hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
+    hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+    hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+    hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+    hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
+    hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+
+    hl.bind("XF86PickupPhone",  hl.dsp.exec_cmd("playerctl previous"),       { locked = true })
+    hl.bind("XF86HangupPhone",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+    hl.bind("XF86Favorites",  hl.dsp.exec_cmd("playerctl next"),   { locked = true })
+
+  '';
+
+  xdg.configFile."hypr/shaders".source = ./shaders;
 
   programs.hyprlock = {
     enable = true;
