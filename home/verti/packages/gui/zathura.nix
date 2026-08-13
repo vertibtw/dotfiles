8db6a8bfeb772a9ac3f54fe8943307548f3d4585
vertibtw <279445040+vertibtw@users.noreply.github.com>
2026-08-13 13:42:29 +0000

    format

--- home/verti/packages/gui/zathura.nix
+++ home/verti/packages/gui/zathura.nix
@@ -1,28 +1,28 @@
 { theme, ... }:
 {
-    programs.zathura = {
-        enable = true;
-        options = {
-            recolor = "true";
-            recolor-darkcolor = theme.colors.text1;
-            recolor-lightcolor = theme.colors.surface0;
+  programs.zathura = {
+    enable = true;
+    options = {
+      recolor = "true";
+      recolor-darkcolor = theme.colors.text1;
+      recolor-lightcolor = theme.colors.surface0;
 
-            default-bg = theme.colors.background;
-            default-fg = theme.colors.text1;
+      default-bg = theme.colors.background;
+      default-fg = theme.colors.text1;
 
-            statusbar-bg = theme.colors.background;
-            statusbar-fg = theme.colors.text1;
+      statusbar-bg = theme.colors.background;
+      statusbar-fg = theme.colors.text1;
 
-            inputbar-bg = theme.colors.background;
-            inputbar-fg = theme.colors.text1;
+      inputbar-bg = theme.colors.background;
+      inputbar-fg = theme.colors.text1;
 
-            notification-bg = theme.colors.background;
-            notification-fg = theme.colors.text1;
+      notification-bg = theme.colors.background;
+      notification-fg = theme.colors.text1;
 
-            highlight-color = theme.colors.blue;
-            highlight-active-color = theme.colors.pink;
+      highlight-color = theme.colors.blue;
+      highlight-active-color = theme.colors.pink;
 
-            selection-notification-color = theme.colors.surface0;
-        };
+      selection-notification-color = theme.colors.surface0;
     };
+  };
 }
