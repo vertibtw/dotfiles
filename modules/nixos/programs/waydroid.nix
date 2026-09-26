dac45b54ce0de34fcf903a86abfe04efeb2a40f0
vertibtw <279445040+vertibtw@users.noreply.github.com>
2026-09-26 17:34:32 +0000

    format

--- modules/nixos/programs/waydroid.nix
+++ modules/nixos/programs/waydroid.nix
@@ -1,22 +1,22 @@
-{ pkgs, inputs, ...}:
+{ pkgs, inputs, ... }:
 {
-    virtualisation.waydroid = {
-        enable = true;
-    };
+  virtualisation.waydroid = {
+    enable = true;
+  };
 
-    systemd.services.waydroid-container = {
-        wantedBy = [ "multi-user.target" ];
-        path = with pkgs; [
-          nettools
-          iproute2
-          iptables
-          dnsmasq
-        ];
-    };
-    environment.systemPackages = with pkgs; [
-        android-tools
-        pkgs.iptables
-        pkgs.dnsmasq
-        inputs.nur.legacyPackages.${pkgs.system}.repos.ataraxiasjel.waydroid-script
+  systemd.services.waydroid-container = {
+    wantedBy = [ "multi-user.target" ];
+    path = with pkgs; [
+      nettools
+      iproute2
+      iptables
+      dnsmasq
     ];
+  };
+  environment.systemPackages = with pkgs; [
+    android-tools
+    pkgs.iptables
+    pkgs.dnsmasq
+    inputs.nur.legacyPackages.${pkgs.system}.repos.ataraxiasjel.waydroid-script
+  ];
 }
